82a859b392d52f182d74f0bae30c4129767940ce
yonatan <yonatanbelay981@gmail.com>
2026-07-20 14:15:45 +0300

    set up EurekaServer

---
+++ infrastructure/eureka-server/src/main/java/com/cinema/eureka_Server/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.cinema.eureka_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
