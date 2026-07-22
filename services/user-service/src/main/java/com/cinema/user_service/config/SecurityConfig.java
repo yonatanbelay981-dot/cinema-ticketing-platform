72698dfbc66d6a2edeafb62a7ba71902a7000f30
yonatan <yonatanbelay981@gmail.com>
2026-07-22 15:48:21 +0300

    Swagger documentation

--- services/user-service/src/main/java/com/cinema/user_service/config/SecurityConfig.java
+++ services/user-service/src/main/java/com/cinema/user_service/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.cinema.user_service;
+package com.cinema.user_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
