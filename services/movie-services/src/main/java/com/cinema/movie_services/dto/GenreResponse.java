cc5269b7aa78ba05f5fc70a2af96ac6996d06ef1
yonatan <yonatanbelay981@gmail.com>
2026-07-24 10:09:43 +0300

    feat:added a dto for the genre entity

---
+++ services/movie-services/src/main/java/com/cinema/movie_services/dto/GenreResponse.java
@@ -0,0 +1,17 @@
+package com.cinema.movie_services.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenreResponse {
+    private UUID id;
+    private String name;
+}
