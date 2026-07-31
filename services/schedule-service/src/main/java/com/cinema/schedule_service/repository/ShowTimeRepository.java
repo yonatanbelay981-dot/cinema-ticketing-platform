e18d64d0f71f4aee13b43c8945790e668adcd56a
yonatan <yonatanbelay981@gmail.com>
2026-07-31 11:51:40 +0300

    feat:(schedule-service) added repository for showtime lifecycle

---
+++ services/schedule-service/src/main/java/com/cinema/schedule_service/repository/ShowTimeRepository.java
@@ -0,0 +1,19 @@
+package com.cinema.schedule_service.repository;
+
+import com.cinema.schedule_service.entity.ScheduleStatus;
+import com.cinema.schedule_service.entity.ShowTime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public interface ShowTimeRepository extends JpaRepository<ShowTime, UUID> {
+   Page<ShowTime> findByMovieId(UUID movieId , Pageable pageable);
+   Page<ShowTime> findByHallId(UUID hallId , Pageable pageable);
+   Page<ShowTime> findByStatus(ScheduleStatus status , Pageable pageable);
+   Page<ShowTime> findByStartTimeBetween(LocalDateTime startTime, LocalDateTime endTime , Pageable pageable);
+   boolean existByHallIdAndStartTimeBetween(UUID hallId, LocalDateTime startTime, LocalDateTime endTime);
+
+}
