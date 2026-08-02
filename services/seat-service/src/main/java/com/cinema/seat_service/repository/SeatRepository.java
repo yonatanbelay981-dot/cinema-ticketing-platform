d9b013d77e8dd67b1c025820c83044e83759cea9
yonatan <yonatanbelay981@gmail.com>
2026-08-02 07:31:03 +0300

    added a repository layer  to seat-service

---
+++ services/seat-service/src/main/java/com/cinema/seat_service/repository/SeatRepository.java
@@ -0,0 +1,17 @@
+package com.cinema.seat_service.repository;
+
+import com.cinema.seat_service.entity.Seat;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SeatRepository  extends JpaRepository<Seat, UUID> {
+
+    Page<Seat> findByHallId(UUID hallId, Pageable pageable);
+    Optional<Seat> findByHallIdAndRowNameAndSeatNumber(UUID hallId, String rowName, Integer seatNumber);
+    boolean existByHallIdAndRowNameAndSeatNumber(UUID hallId, String rowName, Integer seatNumber);
+
+}
