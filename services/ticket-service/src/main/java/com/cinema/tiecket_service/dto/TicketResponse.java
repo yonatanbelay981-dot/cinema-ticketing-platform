14b19a3637b2f342c05c1f5f56c9474e30dbcd2b
yonatan <yonatanbelay981@gmail.com>
2026-08-14 14:28:19 +0300

    feat(ticket-service): add TicketResponse DTO

---
+++ services/ticket-service/src/main/java/com/cinema/tiecket_service/dto/TicketResponse.java
@@ -0,0 +1,23 @@
+package com.cinema.tiecket_service.dto;
+
+import com.cinema.tiecket_service.entity.TicketStatus;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.UUID;
+@Data
+@NoArgsConstructor
+public class TicketResponse {
+
+
+
+        private UUID id;
+
+        private UUID bookingId;
+
+        private String qrCode;
+
+        private TicketStatus status;
+
+        private LocalDateTime generatedAt;
+}
