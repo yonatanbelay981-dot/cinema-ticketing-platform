594ebe7912881308f649051067ce4eeb76936ee3
yonatan <yonatanbelay981@gmail.com>
2026-08-14 14:13:24 +0300

    added a docker compose file

---
+++ services/ticket-service/src/main/java/com/cinema/tiecket_service/TiecketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.tiecket_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiecketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiecketServiceApplication.class, args);
+	}
+
+}
